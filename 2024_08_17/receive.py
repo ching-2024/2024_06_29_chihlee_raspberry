f1fd2b7b3ae3bfbf82ab80bac4389891f10c93a6
ching <ching@test.com>
2024-08-17 16:02:26 +0800

    upload to Render cloud

--- 2024_08_17/receive.py
+++ 2024_08_17/receive.py
@@ -5,11 +5,13 @@ import os
 load_dotenv() #載入.env的
     
 redis_conn = redis.Redis(host=os.environ['REDIS_SERVER'], port=6379, password=os.environ['REDIS_PASS'])
+render_redis_conn = redis.Redis.from_url(os.environ['RENDER_REDIS'])
 
 def on_message(mosq, obj, msg):
     topic = msg.topic
     message = msg.payload.decode('utf-8')
     redis_conn.rpush(topic, message)
+    render_redis_conn.rpush(topic, message)
     print(f"topic={topic}, message:{message}")
           
 if __name__ == '__main__':
